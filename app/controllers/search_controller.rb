df7995345a4806962b37657ab08efa7e5b160488
Tehut Getahun <tehut.m.g@gmail.com>
2017-05-06 15:41:54 -0700

    passed an additional parameter to the .all call in index method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,12 @@ class SearchController < ApplicationController
   require 'edamam'
   def index
    @search =Edamam.new
-   @searches = @search.lookup(params[:query])
+   @searches = @search.all(params[:query], params[:from])
    @search_object = @search.hit_array
   end
 
   def show
-  @search = Edamam.new.single_lookup(params[:uri], params[:from])
+  @search = Edamam.new.find(params[:uri], params[:from])
 
   end
 
