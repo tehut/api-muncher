5205a866da17ad1c4aef0280600043889d6dac0e
Tehut Getahun <tehut.m.g@gmail.com>
2017-05-06 19:19:47 -0700

    rescued EdamamException for no search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,20 @@
-
+require 'edamam'
 class SearchController < ApplicationController
-  require 'edamam'
+
+
   def index
-   @search =Edamam.new
-   @searches = @search.all(params[:query], params[:from])
-   @search_object = @search.hit_array
+    @search =Edamam.new
+    begin
+      @searches = @search.all(params[:query], params[:from])
+      @search_object = @search.hit_array
+    rescue Edamam::EdamamException
+    flash[:message] = "Your search returned no results"
+    render :welcome
+    end
   end
 
   def show
-  @search = Edamam.new.find(params[:uri], params[:from])
+    @search = Edamam.new.find(params[:uri], params[:from])
 
   end
 
