49561c2dd2cc57f323696edc01776ac639858ce4
Tehut Getahun <tehut.m.g@gmail.com>
2017-05-06 19:20:39 -0700

    added data handling for env variables

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
-
+require 'vcr'
+require 'webmock/minitest'
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
@@ -17,8 +18,24 @@ Minitest::Reporters.use!(
 # require "minitest/rails/capybara"
 
 # Uncomment for awesome colorful output
-# require "minitest/pride"
+require "minitest/pride"
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Slack token lying around in a cassette file.
+  config.filter_sensitive_data("<API_KEY>") do
+    ENV['API_KEY']
+  end
+
+  config.filter_sensitive_data("<API_ID>") do
+    ENV['API_ID']
+  end
+end
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
